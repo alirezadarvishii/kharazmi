e6b40915770dc4992c45da0d6d2b8162ef6a7b38
Code With Master <codewithmaster2003@gmail.com>
2021-06-19 07:20:19 +0430

    Develop proje

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-const swiper = new Swiper(".blogs-slider", {
-  slidesPerView: 3,
-  spaceBetween: 25,
+const swiper = new Swiper(".blogs", {
+  slidesPerView: 4,
+  spaceBetween: 20,
   loop: true,
   navigation: {
     nextEl: ".next-slide",
@@ -26,4 +26,14 @@ const sessionsSwiper = new Swiper(".sessions-slider", {
     nextEl: ".next-slide",
     prevEl: ".prev-slide",
   },
+});
+
+const honorsSlider = new Swiper(".honors-slider", {
+  slidesPerView: 4,
+  spaceBetween: 25,
+  loop: true,
+  navigation: {
+    nextEl: ".next-slide",
+    prevEl: ".prev-slide",
+  },  
 });

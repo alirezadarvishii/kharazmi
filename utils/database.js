ab348127bc5d9a436ec0883c98d3ab3240c882c9
Alireza Darvishi <alireza_darvishi_email@yahoo.com>
2022-02-06 17:01:40 +0330

    Refactorin database connection module

--- utils/database.js
+++ utils/database.js
@@ -1,10 +1,17 @@
 const mongoose = require("mongoose");
 
-const connection = mongoose.connect(process.env.MONGODB_URI, {
-  useCreateIndex: true,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
+async function connectDb() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
 
-module.exports = connection;
+module.exports = connectDb;

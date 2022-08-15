44cc1594d1f8b08f2138cd852aecb2bfeb8cce9f
alirezadarvishi <alirezadarvishi.me@gmail.com>
2022-08-15 11:45:57 +0430

    fix:validate middleware bug

--- middleware/validate.js
+++ middleware/validate.js
@@ -1,12 +1,25 @@
+const Joi = require("joi");
+
+const ErrorResponse = require("../utils/ErrorResponse");
 const pick = require("../utils/pick");
 
-module.exports = (schema) => (req, res, next) => {
-  const validate = schema.validate(req);
-  const validFields = pick(validate.value, ["body", "params", "query"]);
-  Object.assign(req, validFields.query);
-  if (!validate.error) {
-    next();
-  } else {
-    res.redirect("/");
+const validate = (schema) => (req, res, next) => {
+  const validSchema = pick(schema, ["params", "query", "body"]);
+  const object = pick(req, Object.keys(validSchema));
+
+  const result = Joi.compile(validSchema)
+    .prefs({ errors: { label: "key" } })
+    .validate(object);
+
+  if (result.error) {
+    const errorMessage = result.error.details
+      .map((details) => details.message)
+      .join(", ");
+    throw new ErrorResponse(422, errorMessage, "back");
   }
+  // eslint-disable-next-line fp/no-mutating-assign
+  Object.assign(req, result.value);
+  next();
 };
+
+module.exports = validate;

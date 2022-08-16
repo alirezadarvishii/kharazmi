ad9f1dc5e43f2cfa16b00641d099b4843d0f3ebf
alirezadarvishi <alirezadarvishi.me@gmail.com>
2022-08-16 22:46:29 +0430

    update contact validation schema

--- validation/contact.validation.js
+++ validation/contact.validation.js
@@ -1,9 +1,21 @@
 const joi = require("joi");
 
-exports.comment = joi.object({
-  fullname: joi.string().required().messages({ "string.empty": "فیلد نام و نام خانوادگی الزامی است!" }),
-  email: joi.string().email().messages({ "string.empty": "فیلد ایمیل الزامی است!" }),
-  phone: joi.string().messages({ "string.empty": "فیلد تلفن تماس الزامی است!" }),
-  content: joi.string().messages({ "string.empty": "فیلد متن پیام الزامی است!" }),
-  _csrf: joi.allow(),
-});
+module.exports.contact = {
+  body: joi.object().keys({
+    fullname: joi
+      .string()
+      .required()
+      .messages({ "string.empty": "فیلد نام و نام خانوادگی الزامی است!" }),
+    email: joi
+      .string()
+      .email()
+      .messages({ "string.empty": "فیلد ایمیل الزامی است!" }),
+    phone: joi
+      .string()
+      .messages({ "string.empty": "فیلد تلفن تماس الزامی است!" }),
+    content: joi
+      .string()
+      .messages({ "string.empty": "فیلد متن پیام الزامی است!" }),
+    _csrf: joi.allow(),
+  }),
+};

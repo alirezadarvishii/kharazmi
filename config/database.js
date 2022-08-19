c1bb60659713a6b4a085a13a295fdfec9d91c5ec
alirezadarvishi <alirezadarvishi.me@gmail.com>
2022-08-19 22:02:33 +0430

    update database config

--- config/database.js
+++ config/database.js
@@ -3,10 +3,8 @@ const mongoose = require("mongoose");
 async function connectDb() {
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
-      useCreateIndex: true,
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useFindAndModify: false,
     });
   } catch (error) {
     console.log(error);

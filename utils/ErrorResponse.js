e2c0afa53704b838981f48b59ccc42a4c9490c6f
alirezadarvishi <alirezadarvishi.me@gmail.com>
2022-08-25 12:45:12 +0430

    delete unnecessary module

--- utils/ErrorResponse.js
+++
@@ -1,9 +0,0 @@
-class ErrorResponse extends Error {
-  constructor(statusCode, message, redirectionPath) {
-    super(message);
-    this.statusCode = statusCode;
-    this.redirectionPath = redirectionPath;
-  }
-}
-
-module.exports = ErrorResponse;

0a0897d2340fbd9746a8b34093cfce6b83f3b213
alirezadarvishi <alirezadarvishi.me@gmail.com>
2022-08-28 12:54:12 +0430

    delete unnecessary comment

--- routes/user.route.js
+++ routes/user.route.js
@@ -5,9 +5,6 @@ const router = Router();
 const userController = require("../controller/user.controller");
 const asyncHandler = require("../middleware/asyncHandler");
 
-// ---------------------- GET ROUTES ----------------------
-
-// Get a user profile.
 router.get("/profile/:role/:userId", userController.profile);
 
 router.post("/approve", asyncHandler(userController.approve));

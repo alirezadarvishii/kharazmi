87a723e3426f27a3512dd0f5093fd771fc23d894
alirezadarvishi <alirezadarvishi.me@gmail.com>
2022-08-28 20:23:23 +0430

    handle sharp errors

--- shared/download-file.js
+++ shared/download-file.js
@@ -1,4 +1,7 @@
 const sharp = require("sharp");
+const httpStatus = require("http-status");
+
+const ApiError = require("../errors/ApiError");
 
 const downloadFile = async (options) => {
   const { buffer, path, quality } = options;
@@ -8,9 +11,13 @@ const downloadFile = async (options) => {
     .then((res) => {
       return res;
     })
-    .catch((err) => {
-      console.log("Sharp Error: ", err);
-      return err;
+    .catch(() => {
+      throw new ApiError({
+        statusCode: httpStatus.BAD_REQUEST,
+        code: httpStatus[400],
+        message: "خطایی در بارگیری تصویر بوجود آمده است!",
+        redirectionPath: "back",
+      });
     });
   return result;
 };

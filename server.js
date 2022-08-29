bc1b69c9b917733667e05e86c82ce39a78d0c049
alirezadarvishi <alirezadarvishi.me@gmail.com>
2022-08-29 20:39:03 +0430

    set default app mode to production

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const createApp = require("./app");
 
 const app = createApp();
 
-const { PORT, NODE_ENV } = process.env;
+const { PORT, NODE_ENV = "production" } = process.env;
 
 app.listen(PORT, () =>
   console.log(`Server Runing On Port: ${PORT}, mode: ${NODE_ENV}! (:`),

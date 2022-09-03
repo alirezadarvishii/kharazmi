2b109302ea8ee656d1624544e532e70fe48818d2
alirezadarvishi <alirezadarvishi.me@gmail.com>
2022-09-03 20:09:15 +0430

    config webpack

--- public/src/js/gallery.js
+++ public/src/js/gallery.js
@@ -1,17 +1,21 @@
-const openGalleryModalBtns = document.querySelectorAll(".gallery .gallery-image");
+const openGalleryModalBtns = document.querySelectorAll(
+  ".gallery .gallery-image",
+);
 const galleryModal = document.querySelector("#galleryImg");
 const modalImg = document.querySelector("#galleryImg img");
 const modalCaption = document.querySelector("#galleryImg .caption");
 const modalLoader = document.querySelector("#galleryImg .spinner-container");
 
-openGalleryModalBtns.forEach((el) =>    
+openGalleryModalBtns.forEach((el) =>
   el.addEventListener("click", (e) => {
     const { imgId } = e.target.dataset;
     galleryModal.dataset.imgId = imgId;
-  })
+  }),
 );
 
-galleryModal.addEventListener("show.bs.modal", () => modalLoader.classList.add("active"));
+galleryModal.addEventListener("show.bs.modal", () =>
+  modalLoader.classList.add("active"),
+);
 galleryModal.addEventListener("shown.bs.modal", async (e) => {
   const { imgId } = e.target.dataset;
   const getImg = await fetch(`http://localhost:3000/gallery/img/${imgId}`);
--- public/webpack.config.js
+++ public/webpack.config.js
@@ -28,12 +28,20 @@ const config = {
     rules: [
       {
         test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /node_modules/,
         use: [
           {
             loader: "babel-loader",
             options: {
-              presets: ["@babel/preset-env"],
+              presets: [
+                [
+                  "@babel/preset-env",
+                  {
+                    useBuiltIns: "usage",
+                    corejs: "3.25",
+                  },
+                ],
+              ],
             },
           },
         ],

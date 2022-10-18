fa2c79cd8fbaa6f41b0dc3f87299f3071d4fd9f2
alirezadarvishi <alirezadarvishi.me@gmail.com>
2022-10-18 23:22:13 +0330

    enable google authentication just for production mode

--- middleware/captcha-verification.js
+++ middleware/captcha-verification.js
@@ -5,26 +5,32 @@ const ApiError = require("../lib/ApiError");
 
 module.exports = async (req, res, next) => {
   const { "g-recaptcha-response": recaptchaResponse } = req.body;
-  try {
-    const captchaVerification = await axios.post(
-      `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.GOOGLE_RECAPTCHA_SECRET_KEY}&response=${recaptchaResponse}`,
-    );
-    if (captchaVerification && captchaVerification.data.success) {
-      next();
-    } else {
+  const { NODE_ENV } = process.env;
+  if (NODE_ENV === "production") {
+    try {
+      const captchaVerification = await axios.post(
+        `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.GOOGLE_RECAPTCHA_SECRET_KEY}&response=${recaptchaResponse}`,
+      );
+      if (captchaVerification && captchaVerification.data.success) {
+        next();
+      } else {
+        throw new ApiError({
+          statusCode: httpStatus.FORBIDDEN,
+          code: httpStatus[403],
+          message:
+            "خطا در احراز هویت کپچا، گزینه من ربات نیستم را فراموش نکنید!",
+          redirectionPath: "back",
+        });
+      }
+    } catch (error) {
       throw new ApiError({
         statusCode: httpStatus.FORBIDDEN,
         code: httpStatus[403],
-        message: "خطا در احراز هویت کپچا، گزینه من ربات نیستم را فراموش نکنید!",
+        message: "خطادر احراز هویت کپچا، گزینه من ربات نیستم را فراموش نکنید!",
         redirectionPath: "back",
       });
     }
-  } catch (error) {
-    throw new ApiError({
-      statusCode: httpStatus.FORBIDDEN,
-      code: httpStatus[403],
-      message: "خطادر احراز هویت کپچا، گزینه من ربات نیستم را فراموش نکنید!",
-      redirectionPath: "back",
-    });
+  } else {
+    next();
   }
 };
